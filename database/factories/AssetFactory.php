1dda6226023daeaee6e5904d34d76e778691f5d3
= <=>
2019-05-01 00:01:57 +0100

    Created  Asset, Roles and Group model

---
+++ database/factories/AssetFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model\Asset;
+use Faker\Generator as Faker;
+
+$factory->define(Asset::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/RolesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model\Roles;
+use Faker\Generator as Faker;
+
+$factory->define(Roles::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
