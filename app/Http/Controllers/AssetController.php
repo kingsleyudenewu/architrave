4f53ccba1aa4aff9350cd31e80643af5fac0acb9
= <=>
2019-05-01 16:06:47 +0100

    refactored fetching assets controller by making the codebase simpler

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Model\Asset;
+use App\Model\Group;
 use App\Model\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AssetController extends Controller
 {
@@ -19,7 +21,15 @@ class AssetController extends Controller
             //Check if the user is an Admin or Not
             $getUserRole = User::with('roles')->where('id', $request->user()->id)->first();
 
-            $getAssets = Asset::with(['groups'])->get();
+            if($getUserRole->roles[0]->name == 'Admin'){
+                $getAssets = User::with(['groups', 'groups.assets'])->get();
+                return $this->successResponse('success', $getAssets);
+            }
+            else{
+                $getAssets = User::with('groups.assets')->where('id', $request->user()->id)->first();
+                return $this->successResponse('success', $getAssets);
+            }
+
         }
         catch (\Exception $exception){
             return $this->errorResponse('Operation failed');
